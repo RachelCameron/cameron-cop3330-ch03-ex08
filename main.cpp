cac522da5a5f9802686ed4d0f1401a4cc35473d5
Rachel Cameron <rachleecameron@gmail.com>
2021-11-22 14:20:29 -0500

    Commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
+#include "header.h"
+using namespace std;
+
+int main() {
+    
+    int value;
+
+    cout << "Please enter an integer value: ";
+    cin >> value;
+
+    if (value %2 == 0) {
+        cout << "The integer value " << value << " is an EVEN number."; }
+    else {
+        cout << "The integer value " << value << " is an ODD number."; }
+    return 0;
+
+}
